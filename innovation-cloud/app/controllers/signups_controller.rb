c840ae5c80b093a976e19ed67d85b9894b8111a9
Jacqueline <maaaander@gmail.com>
2021-01-21 16:02:46 -0500

    finished innovation-cloud

--- innovation-cloud/app/controllers/signups_controller.rb
+++ innovation-cloud/app/controllers/signups_controller.rb
@@ -4,5 +4,22 @@ class SignupsController < ApplicationController
         @signup = Signup.new
     end
 
-    
+
+    private def signup_params
+        params.require(:signup).permit(:email)
+    end
+
+
+    def create
+        @signup = Signup.new(signup_params)
+        if @signup.save 
+            redirect_to '/thanks' 
+        else 
+            render 'new' 
+        end 
+    end 
+
+     
+
+
 end
